a25954be4ec78f69d0c6870d380acc5e5f86a4cc
V <email at email dot email>
2017-05-29 17:10:52 -0700

    shadow dom added

--- DynText.js
+++ DynText.js
@@ -7,8 +7,15 @@ class DynText extends HTMLElement {
 	constructor() {
 		super();
 		console.log("HEY")	
+
+		// Attach a shadow root to the element.
+    	let shadowRoot = this.attachShadow({mode: 'open'});
+    	shadowRoot.innerHTML = `
+      		<style>:host { ... }</style>
+      		<b>in shadow dom!</b>
+      		<slot></slot>
+    	`;
 	}
 }
-
 
 customElements.define('dyn-text', DynText);
